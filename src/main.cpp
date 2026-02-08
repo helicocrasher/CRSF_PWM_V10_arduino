e269d5eea13a46636d5a4d1ac7182b3d1fcc378e
helicocrasher <felix.mobile@gmx.net>
2026-02-08 19:21:46 +0100

    configuration BLUE_PILL working CRSF Baro PWM output

--- src/main.cpp
+++ src/main.cpp
@@ -1,84 +1,116 @@
 #include <arduino.h>
 #include <Wire.h>
-#include <SPI.h>
-#include <NullSerial.h>
 
+#include <SPI.h>
+
+#include <NullSerial.h>     // NullSerial for disabling debug output
 #include <AlfredoCRSF.h>
 
+#include "hardware.h"       //required for Hardware stuff
+
 AlfredoCRSF crsf;
+TwoWire BaroWire(SDA1, SCL1);	// I2C bus for Barometer sensor SDA1 & SCL1 defined in hardware.h
 
 void setupBaroSensor();
+void setup_HW_OUT_PWM(void);
+void setPWMChannels();
 static void telemetrySendCellVoltage(uint8_t cellId, float voltage);
 void telemetrySendBaroAltitude(float altitude, float verticalspd);
 void baroProcessingTask(uint32_t millis_now);
 void baroSerialDisplayTask(uint32_t millis_now);
 void getI2cData(uint8_t I2C_Address,uint8_t device_register  , uint8_t num_bytes,short * indata);
+void printChannels();
+
+  #define STRING_BUFFER_SIZE 120
+  char stringBuffer[STRING_BUFFER_SIZE]={0};
+
+#if !defined(STM32_CORE_VERSION) || (STM32_CORE_VERSION  < 0x01090000)
+#error "Due to API change, this sketch is compatible with STM32_CORE_VERSION  >= 0x01090000"
+#endif
 
 #ifdef TARGET_BLUEPILL        // and BMP280  sensor
   #include <Adafruit_BMP280.h>
   #include "SparkFun_u-blox_GNSS_Arduino_Library.h" 
-  #define LED_BUILTIN PB2
-  //HardwareSerial Serial1(USART1); // somewhere in arduino already defined for the Bluepill board
-  HardwareSerial Serial2(USART2);
-  HardwareSerial Serial3(USART3);
-  #define crsfSerial Serial1
+
+  #define SerialDebug Serial2
+//  #define SerialI2CDebug NullSerial  
   #define SerialI2CDebug Serial2
-  #define GNSSSerialDebug Serial2
+  #define GNSSSerialDebug NullSerial
+
   #define gnssSerial Serial3
-  #define CRF_SERIAL_TX_PIN PB6  // USART1 TX
-  #define CRF_SERIAL_RX_PIN PB7  // USART1 RX
+
   #define GNSS_SERIAL Serial3
-  #define GNSS_SERIAL_TX_PIN PB10  // USART3 TX
-  #define GNSS_SERIAL_RX_PIN PB11  // USART3 RX
+  #define GNSS_SERIAL_TX_PIN SERIAL3_TX  // USART3 TX
+  #define GNSS_SERIAL_RX_PIN SERIAL3_RX  // USART3 RX
   uint8_t BaudRateIndex=0;
   #define NR_OF_GNSS_baudrates 5
   const uint32_t possibleBauds[NR_OF_GNSS_baudrates] = { 115200, 57600, 38400, 19200, 9600 };
-  TwoWire BaroWire(PB9, PB8);             // define SDA,SCL pins for BMP280 Baro sensor
-  Adafruit_BMP280 Baro_Sensor(&BaroWire); // 
+             // define SDA,SCL pins for BMP280 Baro sensor
+  Adafruit_BMP280 BaroSensor(&BaroWire); // 
   SFE_UBLOX_GNSS myGNSS;
   bool autoBaudGNSS(void);
   void sendGps_int(int32_t latitude, int32_t longitude, int32_t groundspeed, int32_t heading, int32_t altitude, uint8_t satellites);
   void printGNSS(void);
   bool GNSS_available=false;
-#define STRING_BUFFER_SIZE 120
-  char stringBuffer[STRING_BUFFER_SIZE]={0};
-  #define mmsTokmh 278        // conversion factor from mm/s to km/h
+
+  #define mmsTokmh 278          // conversion factor from mm/s to km/h
+  #define NR_OF_PWM_OUT 8
+  //uint32_t PWM_Tim_Pin_Map[NR_OF_PWM_OUT] = {PA0,PA1,PB10,PB11,PA6,PA7_ALT1,PB0_ALT1,PB1_ALT1};
+  #define TIMER_PRESCALER 72    // 72MHZ STM32 clock div 72 --> timer clock 1MHz
+  #define PWM_PERIOD_US 20000   // 20ms period for 50Hz
 #endif  // TARGET_BLUEPILL and BMP280 sensor
 
 #ifdef TARGET_MATEK_CRSF_PWM_V10        // TARGET_MATEK_CRSF_PWM_V10 with SPL06-001
   #include <SPL06-001.h>
-  #define LED_BUILTIN PC14
-  HardwareSerial Serial1(USART1); 
-  HardwareSerial Serial2(USART2);
-  #define crsfSerial Serial1
+
+
   #define SerialI2CDebug Serial2
-  #define CRF_SERIAL_TX_PIN PB6   // USART1 TX
-  #define CRF_SERIAL_RX_PIN PB7   // USART1 RX
-  TwoWire BaroWire(PA12, PA11);   // CRSF PWM V10 SDA,SCL
-  SPL06 Baro_Sensor(&BaroWire);   // 
+
+  SPL06 BaroSensor(&BaroWire);   // 
 #endif
 
+#define crsfSerial Serial1
+#define CRSF_SERIAL_TX_PIN SERIAL1_TX
+#define CRSF_SERIAL_RX_PIN SERIAL1_RX
+
+uint32_t Tim_out_channel[NR_OF_PWM_OUT];
+HardwareTimer *My_PWM_Out_Tim[NR_OF_PWM_OUT];
 
 void setup() {
-  SerialI2CDebug.begin(115200);
+  SerialDebug.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
   #ifdef TARGET_BLUEPILL
-   // while (!Serial) ;  // wait for serial port to connect. Needed for native USB
+//  while (!Serial) ; // wait for serial port to connect. Needed for native USB
   #endif
   SerialI2CDebug.println("Setup Baro Sensor starting");
   delay(100);    
   setupBaroSensor();
   delay(100);
-  crsfSerial.setTx(CRF_SERIAL_TX_PIN);
-  crsfSerial.setRx(CRF_SERIAL_RX_PIN); 
+  crsfSerial.setTx(CRSF_SERIAL_TX_PIN);
+  crsfSerial.setRx(CRSF_SERIAL_RX_PIN); 
   delay(100);
   crsfSerial.begin(CRSF_BAUDRATE, SERIAL_8N1);
   crsf.begin(crsfSerial);
   delay(100);
-#ifdef TARGET_BLUEPILL
+  setup_HW_OUT_PWM();
+  #ifdef TARGET_BLUEPILL
   GNSS_available = autoBaudGNSS();
-#endif
+  if (GNSS_available) {
+    myGNSS.setMeasurementRate(250);     // Set the GNSS module to 1 second (1000 ms) measurement rate
+    myGNSS.saveConfiguration();
+    myGNSS.setAutoPVT(true);            // Enable automatic PVT data messages
+//    myGNSS.enableDebugging();           // Uncomment this line to enable helpful debug messages on Serial
+    myGNSS.setUART1Output(COM_TYPE_UBX); //Set the UART1 port to output UBX only (turn off NMEA noise)
+  }
+  #endif
+  SerialDebug.print("F_CPU = ");
+  SerialDebug.print(F_CPU);
+  SerialDebug.print("   HSE_VALUE = ");
+  SerialDebug.println(HSE_VALUE);
+  SerialDebug.println("Setup done");
+//  loop_counter=0;
+//  micros_last=millis();
 }
 
 #define IIR_ALPHA   0.135755f  // 0.5Hz cut off (fc/40 / fc=Hz /Tc=320ms)
@@ -91,45 +123,66 @@ static double filt_vario=0, filt_alt_ASL=0;
 static uint32_t GND_alt_count=0;
 
 void loop() {
-
 static uint32_t millis_now=0, millis_last=0,main_loop_counter=0,crsf_last_update=0;
 
   millis_now = millis();
-  if(millis_now - millis_last > 250) {
+  if(millis_now - millis_last >= 500){  // every 500ms{
     millis_last = millis_now;
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    printChannels();
     telemetrySendCellVoltage(1, 4.11);
     telemetrySendBaroAltitude(filt_alt_AGL, filt_vario);
-#ifdef TARGET_BLUEPILL  
+  #ifdef TARGET_BLUEPILL
     if(GNSS_available) {
       sendGps_int(myGNSS.getLatitude(), myGNSS.getLongitude(), myGNSS.getGroundSpeed()/mmsTokmh, myGNSS.getHeading(), myGNSS.getAltitudeMSL()/1000, myGNSS.getSIV());
       printGNSS();
+
     }
 #endif
   }
-  baroProcessingTask(millis_now);
-  baroSerialDisplayTask(millis_now);
+//  baroProcessingTask(millis_now);
+//  baroSerialDisplayTask(millis_now);
 
-//  if (millis_now-crsf_last_update > 0){
+  if (millis_now-crsf_last_update > 100){
+#ifdef TARGET_BLUEPILL
+    if(GNSS_available) myGNSS.getPVT();
+    crsf_last_update = millis_now;
+#endif
+  }
   if (true){
     crsf.update();
-    crsf_last_update = millis_now;
+    setPWMChannels();
+
   }
-#ifdef TARGET_BLUEPILL
-  if(GNSS_available) myGNSS.getPVT();
-#endif
+
   main_loop_counter++;
+}
+
+void setPWMChannels() {
+  for (int PWM_channelIndex = 0; PWM_channelIndex < NR_OF_PWM_OUT; PWM_channelIndex++)
+  {
+    My_PWM_Out_Tim[PWM_channelIndex]->setCaptureCompare(Tim_out_channel[PWM_channelIndex], crsf.getChannel(PWM_channelIndex+1), TICK_COMPARE_FORMAT);
+  }
+}
+
+void printChannels() {
+  for (int ChannelNum = 1; ChannelNum <= NR_OF_PWM_OUT; ChannelNum++)
+  {
+    SerialDebug.print(crsf.getChannel(ChannelNum));
+    SerialDebug.print(", ");
+  }
+  SerialDebug.println(" ");
 }
 
 void baroSerialDisplayTask(uint32_t millis_now){
   static uint32_t last_millis=0;
-  if (millis_now - last_millis < 250) return;
+  if (millis_now - last_millis < 2500) return;
   last_millis = millis_now;
   SerialI2CDebug.print(F("Temperature = "));
-  SerialI2CDebug.print(Baro_Sensor.readTemperature());
+  SerialI2CDebug.print(BaroSensor.readTemperature());
   SerialI2CDebug.println(" *C");
   SerialI2CDebug.print(F("Pressure = "));
-  SerialI2CDebug.print(Baro_Sensor.readPressure());
+  SerialI2CDebug.print(BaroSensor.readPressure());
   SerialI2CDebug.println(" Pa");
   SerialI2CDebug.print(F("Approx altitude ASL = "));    
   SerialI2CDebug.print(filt_alt_ASL);
@@ -146,13 +199,13 @@ void baroSerialDisplayTask(uint32_t millis_now){
 void baroProcessingTask(uint32_t millis_now){
   float altitude;
   static uint32_t last_millis=0,loop_counter=0;
-  if (millis_now - last_millis < 50) return;
+  if (millis_now - last_millis < 100) return;
   last_millis = millis_now;
 
   #ifdef TARGET_BLUEPILL  // BMP280 sensor
-    altitude = Baro_Sensor.readAltitude(1013.25); /* Adjusted to local forecast! */
+    altitude = BaroSensor.readAltitude(1013.25); /* Adjusted to local forecast! */
   #else                   // SPL06-001 sensor
-    altitude = Baro_Sensor.readPressureAltitudeMeter(1013.25);
+    altitude = BaroSensor.readPressureAltitudeMeter(1013.25);
   #endif
   if (loop_counter <1000) {    // initialisation --> average ground altitude
     GND_altitude += altitude;
@@ -184,7 +237,7 @@ void bla(uint32_t millis_now){
 
 #define CRSF_BATTERY_SENSOR_CELLS_MAX 12
 
-void telemetrySendCellVoltage(uint8_t cellId, float voltage) {
+static void telemetrySendCellVoltage(uint8_t cellId, float voltage) {
   if (cellId < 1 || cellId > CRSF_BATTERY_SENSOR_CELLS_MAX)     return;
 
   uint8_t payload[3];
@@ -213,6 +266,42 @@ void telemetrySendBaroAltitude(float altitude, float verticalspd)
 }
 
 
+void setup_HW_OUT_PWM(void)
+{
+  // no need to configure pin, it will be done by HardwareTimer configuration
+
+  uint32_t PrescalerFactor = TIMER_PRESCALER;
+
+  // Automatically retrieve TIM instance and channel associated to pin
+  // This is used to be compatible with all STM32 series automatically.
+  for (int PWM_ChannelIndex=0; PWM_ChannelIndex<NR_OF_PWM_OUT;PWM_ChannelIndex++){
+    TIM_TypeDef *Instance = (TIM_TypeDef *)pinmap_peripheral(digitalPinToPinName(PWM_Tim_Pin_Map[PWM_ChannelIndex]), PinMap_PWM);
+    SerialDebug.println();
+    SerialDebug.print("Configuring PWM output on pin: ");
+    SerialDebug.println(digitalPinToPinName(PWM_Tim_Pin_Map[PWM_ChannelIndex]),HEX);
+    //Instantiate HardwareTimer object. Thanks to 'new' instantiation, HardwareTimer is not destructed when setup() function is finished.
+    My_PWM_Out_Tim[PWM_ChannelIndex] = new HardwareTimer(Instance);
+    sprintf(stringBuffer," %08X", (void *)My_PWM_Out_Tim[PWM_ChannelIndex]);
+    SerialDebug.println(stringBuffer);
+
+    My_PWM_Out_Tim[PWM_ChannelIndex]->setPrescaleFactor(PrescalerFactor);
+    My_PWM_Out_Tim[PWM_ChannelIndex]->setOverflow(PWM_PERIOD_US,TICK_FORMAT);   // Set period in microseconds
+    SerialDebug.print("timer for "); SerialDebug.print(PWM_Tim_Pin_Map[PWM_ChannelIndex],HEX); SerialDebug.println(" configured");
+
+  
+    Tim_out_channel[PWM_ChannelIndex] = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(PWM_Tim_Pin_Map[PWM_ChannelIndex]), PinMap_PWM));
+    sprintf(stringBuffer,"PWM_ChannelIndex = %02d, Tim_Out_Channel = %02d, Pin_Map= %04X", PWM_ChannelIndex, Tim_out_channel[PWM_ChannelIndex],PWM_Tim_Pin_Map[PWM_ChannelIndex]);
+    SerialDebug.println(stringBuffer);
+
+    My_PWM_Out_Tim[PWM_ChannelIndex]->setMode(Tim_out_channel[PWM_ChannelIndex], TIMER_OUTPUT_COMPARE_PWM1, PWM_Tim_Pin_Map[PWM_ChannelIndex]);
+    My_PWM_Out_Tim[PWM_ChannelIndex]->setCaptureCompare(Tim_out_channel[PWM_ChannelIndex], (uint32_t) (1500+PWM_ChannelIndex*100), TICK_COMPARE_FORMAT);
+  
+    My_PWM_Out_Tim[PWM_ChannelIndex]->resume();
+    sprintf(stringBuffer,"timer channel %d started",PWM_ChannelIndex);
+    SerialDebug.println(stringBuffer);
+  }
+}
+
 void getI2cData(uint8_t I2C_Address,uint8_t device_register  , uint8_t num_bytes,short * indata) {
   uint8_t bytes_read;
   Wire.begin();  
@@ -239,11 +328,11 @@ void setupBaroSensor(){   // BMP280 sensor version
   unsigned status;
   BaroWire.begin();
   //status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
-  status = Baro_Sensor.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
+  status = BaroSensor.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
   if (!status) {
     SerialI2CDebug.println(F("Could not find a valid BMP280 sensor, check wiring or "
                       "try a different address!"));
-    SerialI2CDebug.print("SensorID was: 0x"); SerialI2CDebug.println(Baro_Sensor.sensorID(),16);
+    SerialI2CDebug.print("SensorID was: 0x"); SerialI2CDebug.println(BaroSensor.sensorID(),16);
     SerialI2CDebug.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
     SerialI2CDebug.print("        ID of 0x56-0x58 represents a BMP 280,\n");
     SerialI2CDebug.print("        ID of 0x60 represents a BME 280.\n");
@@ -252,7 +341,7 @@ void setupBaroSensor(){   // BMP280 sensor version
   }
 
   /* Default settings from datasheet. */
-  Baro_Sensor.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+  BaroSensor.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
                   Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
                   Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
@@ -388,16 +477,16 @@ void setupBaroSensor(){   // SPL06-001 sensor version
 
   delay(10);
   
-  status = Baro_Sensor.begin(SPL06_ADDRESS_ALT,SPL06_PRODID);
-  //unsigned status = Baro_Sensor.begin(SPL06_ADDRESS_ALT);
+  status = BaroSensor.begin(SPL06_ADDRESS_ALT,SPL06_PRODID);
+  //unsigned status = BaroSensor.begin(SPL06_ADDRESS_ALT);
   if (!status) {
     SerialI2CDebug.println(F("Could not find a valid SPL06-001 sensor, check wiring or "
                       "try a different address!"));
-    SerialI2CDebug.print("SensorID was:   0x"); SerialI2CDebug.println(Baro_Sensor.sensorID(),HEX); //HEX
+    SerialI2CDebug.print("SensorID was:   0x"); SerialI2CDebug.println(BaroSensor.sensorID(),HEX); //HEX
     SerialI2CDebug.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
     while (1) delay(10);
   }
-  Baro_Sensor.setSampling(SPL06::MODE_BACKGND_BOTH,
+  BaroSensor.setSampling(SPL06::MODE_BACKGND_BOTH,
                           SPL06::SAMPLING_X16,
                           SPL06::SAMPLING_X16,
                           SPL06::RATE_X16,
